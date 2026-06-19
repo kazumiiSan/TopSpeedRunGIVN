de7ca39eb1d5719f345d1d4250880ed625769672
kazumiiSan <kazumi150904@gmail.com>
2026-06-19 12:08:27 +0700

    Update homeController.php

--- app/controllers/homeController.php
+++ app/controllers/homeController.php
@@ -1,1 +1,14 @@
+<?php
+class homeController extends Controller {
+    public function index() {
+        // Bạn có thể lấy dữ liệu từ Model ở đây nếu cần
+        // Ví dụ: $games = $this->model('GameModel')->getTopGames();
+        
+        $data = [
+            'title' => 'TopSpeedRunGIVN - Trang Chủ'
+        ];
 
+        // Gọi view và truyền dữ liệu qua
+        $this->view('home/index', $data);
+    }
+}
